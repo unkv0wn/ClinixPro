d96a62489466da7d508500c46dbefc5e6c624df1
unkv0wn <cjeanb0@gmail.com>
2025-03-19 19:17:14 -0300

    🎉 Initial configs in the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LogoComponent } from "./components/logo/logo.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [/*RouterOutlet*/ LogoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'clinixpro';
+  title = 'ClinxPRO';
 }
---
+++ src/app/components/logo/logo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'logo-component',
+  imports: [],
+  templateUrl: './logo.component.html',
+
+})
+export class LogoComponent {
+
+}
