1608fdf7033755a3b82b18f1b521a643133c9eab
unkv0wn <cjeanb0@gmail.com>
2025-03-25 16:50:24 -0300

    💄 Update front end
    Create a component in home page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-     { path: '', component: LoginComponent },
-     { path: 'index', component: HomeComponent },
-     { path: 'agendamentos', component: SchedulingComponent },
-     { path: 'financeiro', component: FinancalComponent },
+  { path: '', component: LoginComponent },
+  { path: 'index', component: HomeComponent },
+  { path: 'agendamentos', component: SchedulingComponent },
+  { path: 'financeiro', component: FinancalComponent },
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  LucideAngularModule,
+  Clock,
+  Plus,
+  NotebookPen,
+  ChevronLeft,
+  ChevronDown,
+  Pin,
+  Trash2,
+  Trash,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
+  standalone: true,
+  imports: [LucideAngularModule, CommonModule],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  readonly Clock = Clock;
+  readonly Plus = Plus;
+  readonly NotebookPen = NotebookPen;
+  readonly ChevronLeft = ChevronLeft;
+  readonly ChevronDown = ChevronDown;
+  readonly Pin = Pin;
+  readonly Trash = Trash2;
+
+  appointments = [
+    { time: '08:00' },
+    { time: '08:30' },
+    { time: '09:00' },
+    { time: '11:00' },
+    { time: '12:00' },
+    // ...add more appointments as needed
+  ];
+
+  notions = [
+    { message: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum' },
+    { message: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum' },
+    { message: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum' },
+    { message: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum' },
+    { message: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum' },
+  ];
+
+  constructor() {}
+}
