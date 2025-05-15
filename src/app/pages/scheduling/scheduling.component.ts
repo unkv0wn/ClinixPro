db762718cb7f250beef65703ee8944fcd7ae858a
unkv0wn <cjeanb0@gmail.com>
2025-05-15 19:41:37 -0300

    💄 Create a button to new consult in scheduling

--- src/app/pages/scheduling/scheduling.component.ts
+++ src/app/pages/scheduling/scheduling.component.ts
@@ -1,22 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LucideAngularModule, Users, Camera, Search, Trash2, Printer, FileText } from 'lucide-angular';
+import {
+  LucideAngularModule,
+  Users,
+  Camera,
+  Search,
+  Trash2,
+  Printer,
+  FileText,
+  Plus,
+} from 'lucide-angular';
 import { IconHeaderComponent } from '../../components/icon-header/icon-header.component';
+import { NewConsultComponent } from '../../components/modals/new-consult/new-consult.component';
 
 @Component({
   selector: 'app-scheduling',
-  imports: [LucideAngularModule, CommonModule, IconHeaderComponent],
+  imports: [
+    LucideAngularModule,
+    CommonModule,
+    IconHeaderComponent,
+    NewConsultComponent,
+  ],
   templateUrl: './scheduling.component.html',
-  styleUrl: './scheduling.component.css'
-
+  styleUrl: './scheduling.component.css',
 })
 export class SchedulingComponent {
   readonly Users = Users;
   readonly Camera = Camera;
   readonly Seach = Search;
-  readonly Trash = Trash2
+  readonly Trash = Trash2;
   readonly Printer = Printer;
   readonly TableOfContents = FileText;
+  readonly Plus = Plus;
+
+  isModal: boolean = false;
+  isModalConsult: boolean = false;
 
   notions = [
     { message: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum' },
@@ -36,4 +54,7 @@ export class SchedulingComponent {
     { message: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum' },
   ];
 
+  ShowModalConsult() {
+    this.isModalConsult = !this.isModalConsult;
+  }
 }
